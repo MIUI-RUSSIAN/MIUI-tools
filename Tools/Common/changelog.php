5a68e0433a3d64cc27c7c296ba6401121988591d
mnlk2 <mottier.ludovic@free.fr>
2011-02-27 00:04:27 +0100

    Update vers 1.2.25

--- Tools/Common/changelog.php
+++ Tools/Common/changelog.php
@@ -54,8 +54,55 @@ h1 {
 </head>
 <body>
 <h2>1.2.19</h2>
-<li>correction de bug dont le lag rencontrer sur certain tel, mais pas de changelog officiel
+<h1>Téléphone:</h1>
+<li>Ajouter les appels d'attribution de soutenir le code de pays étrangers
+<li>Ajouter augmentation de la demande afin de déterminer le renvoi d'appel et des conseils
 
+<h1>SMS:</h1>
+<li>Fixer le problème ne peut pas recevoir des SMS 10086
+<li>message de réponse de réparation ne pas afficher les mots pop-up problème fenêtre
+<li>audio multimédia et vidéo réparation le problème en bloquant
+<li>Aucun étranger à la réparation des récents contacts affichage
+<li>Réparez ou enlevez la carte SIM de téléphone le nom du contact est vide, le problème FC
+<li>Réparation des problèmes de connexion Bluetooth peut se produire FC
+<li>Fix d'importation / supprimer des contacts de la carte SIM, appuyez sur HOME clé lorsqu'il rentre le problème FC
+
+<h1>Anti-déranger:</h1>
+<li>liste noire FC entrer dans un espace va régler le problème
+
+<h1>Paramètres:</h1>
+<li>Ajouter à régler l'heure d'alarme est réglé pour ajouter l'entrée en
+
+<h1>Thème:</h1>
+<li>Ajouter le sujet pour parcourir le contenu de la page ajouter un bouton ou le sujet suivant
+<li>Fix pour résoudre les paramètres de fonds d'écran dynamiques mal réglé pour le papier peint bug en cours
+<li>Kit de réparation le fond d'écran de verrouillage, l'appel ne peut pas définir les problèmes de blocage logiciel écran
+<li>Réparation des icônes locales ne changent pas l'icône de dossier pour le bug
+
+<h1>Verrouiller l'écran, la barre d'état:</h1>
+<li>En vertu de la durée optimale de l'écran de verrouillage, appuyez sur le bouton d'alimentation ne se déclenche pas au large de la fenêtre pop-up
+<li>Optimisation pour améliorer la serrure pour déverrouiller l'écran lorsque vous écoutez de la musique sous l'efficacité
+<li>Réparation à la même "China Mobile" show time et "China Telecom" question
+<li>Réparation du téléphone, sous le coin supérieur droit de l'écran de verrouillage ne montre pas une question de temps
+<li>Réparation ou remplacement de rappeler son plus doux de faible puissance
+
+<h1>Musique:</h1>
+<li>Ajouter écouté télécharger, écouter les chansons en ligne à télécharger sur un réseau local (à ouvrir dans les paramètres)
+<li>Ajouter l'association automatique des paroles locales et des pochettes d'album, le même nom de dossier directement liés à la
+<li>Ajouter la page pour actualiser le menu pour ajouter la bibliothèque de commutateur
+<li>Ajouter nouveau jeu de paroles et pochette de l'album montre l'interrupteur (peut être mis en valeur par les paroles et l'écran de façade)
+<li>solution de réparation brouillé lyrics
+<li>page Album utilisation de la mémoire d'optimiser
+
+<h1>Appareil photo:</h1>
+<li>Optimisation des icônes opération, avec la rotation de la rotation de la caméra
+
+<h1>Galerie:</h1>
+<li>Ajouter sept effets d'animation: Fade, table tournante, flip, pile, faire pivoter, boîte, ventilateur. Comme les six premiers effets et de bureau, le secteur est ajouté. Défini dans le menu des paramètres à l'intérieur du
+<li>Ajouter des effets d'animation et de glissement peut être défini retard, dans le menu paramètres dans la définition de
+<li>Galerie Photo plug-in de soutien pour les trois tailles: 2x2, 2x4, 4x4
+<li>Large coulissant autour d'optimisation pour améliorer la fluidité
+<li>Une sorte de réparation FC dossier 
 
 <div id="footer"> 
   <h3>mnlk2</h3>
